fcab4acec9ffd147f3203cab1fe64af6d2f4ca9a
Sergio D. Barrera <sdbeng@gmail.com>
2021-11-06 09:41:46 -0700

    update reducer test

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -4,6 +4,7 @@ import {
     UPDATE_CATEGORIES,
     UPDATE_CURRENT_CATEGORY
 } from '../utils/actions'
+import { reducer } from '../utils/reducers'
 
 //create a sample of what our global state will look like
 const initialState = {
